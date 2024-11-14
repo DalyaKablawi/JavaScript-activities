6cbd043ff1415155f8ccbebab94a165b0adb2c37
dalya kablawi <dalyakablawi@gmail.com>
2024-11-13 23:19:37 -0500

    final activity 2

--- JavaScript-activities/activity-02.js
+++ JavaScript-activities/activity-02.js
@@ -1,2 +1,17 @@
-const personName = "Dalya";
+let personName = "Dalya";
 console.log(`My name is ${personName}`);
+
+let pets = 3;
+console.log(`I have ${pets} pets`);
+
+let funFact = "I can speak Korean";
+console.log(`Fun fact: ${funFact}`);
+
+personName = "Deanna";
+console.log(`My partner's name is ${personName}`);
+
+pets = 4;
+console.log(`I have ${pets} pets`);
+
+funFact = "She can speak Spanish";
+console.log(`Fun fact: ${funFact}`);
