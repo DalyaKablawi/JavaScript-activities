a43803a4658500598adba91c7f07fd615e6ec296
dalya kablawi <dalyakablawi@gmail.com>
2024-11-14 22:11:08 -0500

    solved activity 4

--- JavaScript-activities/activity-04.js
+++ JavaScript-activities/activity-04.js
@@ -1,11 +1,11 @@
 const a = "50";
 const b = 50;
 const c = 100;
-const d = c % b;
-const e = c / 2;
+const d = c % b; // 0
+const e = c / 2; // 50
 
-const expression1 = b === e;
-const expression2 = e < d;
+const expression1 = b === e; // TRUE
+const expression2 = e < d; // FALSE
 
 // Use comparison operators so all expressions below log to the console as true
 console.log(a === b);
@@ -14,5 +14,5 @@ console.log(c < b);
 console.log(d > 0);
 
 // Use logical operators so all expressions below log to the console as true
-console.log(expression1 && expression2);
-console.log(!expression1 || expression2);
+console.log(expression1 || expression2); // change && to ||
+console.log(expression1 || expression2); // remove !
