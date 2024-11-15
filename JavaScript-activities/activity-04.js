2106e4852d925580f3a97e934ef66214dacd09e8
dalya kablawi <dalyakablawi@gmail.com>
2024-11-14 22:12:48 -0500

    solved activity 4

--- JavaScript-activities/activity-04.js
+++ JavaScript-activities/activity-04.js
@@ -8,10 +8,10 @@ const expression1 = b === e; // TRUE
 const expression2 = e < d; // FALSE
 
 // Use comparison operators so all expressions below log to the console as true
-console.log(a === b);
-console.log(b !== e);
-console.log(c < b);
-console.log(d > 0);
+console.log(a == b);
+console.log(b === e);
+console.log(c > b);
+console.log(d === 0);
 
 // Use logical operators so all expressions below log to the console as true
 console.log(expression1 || expression2); // change && to ||
