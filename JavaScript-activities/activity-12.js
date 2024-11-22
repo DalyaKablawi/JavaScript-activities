9fb71b5f772b79c2e9b6c118bf1a22d1fdb2863b
dalya kablawi <dalyakablawi@gmail.com>
2024-11-21 21:54:35 -0500

    saving changes

---
+++ JavaScript-activities/activity-12.js
@@ -0,0 +1,7 @@
+let students = ["Student1", "Student2", "Student3"];
+
+console.log(`The total number of elements in the array is ${students.length}`);
+
+for (let i = 0; i < students.length; i++) {
+  console.log(`Welcome to the class, ${students[i]}`);
+}
